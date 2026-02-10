5350596d9027782197d69307dd09a2f5718b15e6
thakkarprem751-wq <thakkarprem751@gmail.com>
2026-02-10 15:04:35 +0000

    wrote add_two_numbers and print_hello_world

---
+++ python/computing_with_formulas/add_two_numbers.py
@@ -0,0 +1,7 @@
+first_number = 5.
+print(f'Type of first number is: (type(first_number))')
+second_number = 7
+
+sumation = first_number + second_number 
+print(f' The sum of (first_number) and (second_number) is (sum).')
+
